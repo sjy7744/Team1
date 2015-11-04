4324facdcd2fb1c308e2fafa51d476580e83f76f
GUGUDAN <lizingmaster@naver.com>
2015-11-04 19:26:18 +0900

    구구단 함수 작성했습니다.

--- HW7/src/Main.java
+++ HW7/src/Main.java
@@ -1,8 +1,21 @@
 
+import java.util.*;
 public class Main {
 
 	public static void main ( String[] args ) {
-
+		
 	}
-	
+	public static void GUGUDAN(){
+		Scanner sc = new Scanner(System.in);
+		int num;
+		System.out.println("구구단 입니다.");
+		System.out.println("숫자를 입력하세요.");
+		num = sc.nextInt();
+		System.out.println(num + "단 출력합니다.");
+		for(int i = 1; i <10; i ++)
+		{
+			System.out.println(num +"*" +i +"="+ num*i);
+		}
+		
+	}
 }
