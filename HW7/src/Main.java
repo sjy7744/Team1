bf173b452b41539d055539208a6da0e5fe118b89
Xnote <Xnote@Xnote-PC>
2015-11-04 19:28:23 +0900

    drawDiamond

--- HW7/src/Main.java
+++ HW7/src/Main.java
@@ -2,7 +2,23 @@
 public class Main {
 
 	public static void main ( String[] args ) {
-
+		drawDiamond();
 	}
-	
+	public static void drawDiamond(){
+		System.out.println("Hello Git!");
+		for(int i=1;i<=4;i++){
+			for(int j=1;j<=5-i;j++){
+			System.out.print("¡¡");}
+			for(int j=1;j<=(i*2)-1;j++){
+			System.out.print("¡ß");}
+		System.out.println();
+		}
+		for(int i=5;i>=1;i--){
+			for(int j=5-i;j>=1;j--){
+			System.out.print("¡¡");}
+			for(int j = (i*2)-1;j>=1;j--){
+				System.out.print("¡ß");}
+			System.out.println();
+		}
+	}
 }
