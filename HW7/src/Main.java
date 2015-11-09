a5f614f9e81aaaef05a714dfa886247cd6272e07
mhkang94 <lizingmaster@naver.com>
2015-11-09 17:23:14 +0900

    Update Main.java

--- HW7/src/Main.java
+++ HW7/src/Main.java
@@ -1,6 +1,9 @@
 
 import java.util.*;
 public class Main {
+	public static void main ( String[] args ) {
+		
+	}
 	public static void GUGUDAN(){
 		Scanner sc = new Scanner(System.in);
 		int num;
@@ -14,9 +17,4 @@ public class Main {
 		}
 		
 	}
-	
-	public static void main ( String[] args ) {
-		
-	}
-
 }
