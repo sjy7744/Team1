fcd1de02e511946e38e78daa90b30a23387cf33b
손지영 <sjy7744@naver.com>
2015-11-10 15:43:04 +0900

    현재시간을 출력하는 내용을 구현하였습니다.

---
+++ HW7/src/Main.java
@@ -0,0 +1,19 @@
+import java.text.DateFormat;
+import java.util.Date;
+
+public class Main {
+
+	public static void main ( String[] args ) {
+		
+		System.out.println("Current Time is : " + getTime());
+	}
+	
+	public static String getTime() {
+			Date date = new Date();
+			DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG);
+			
+				String formattedDate = dateFormat.format(date);
+				
+				return formattedDate;
+		 	}
+}
